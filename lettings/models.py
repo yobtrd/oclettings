5d9a14ad45572bb0636da5fcce332a42bb58f835
yobtrd <gajy@tutamail.com>
2026-08-14 14:48:03 +0200

    refactor: split lettings and profiles into separate apps

--- lettings/models.py
+++ lettings/models.py
@@ -1,4 +1,3 @@
-from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinLengthValidator
 from django.db import models
 
@@ -12,7 +11,7 @@ class Address(models.Model):
     country_iso_code = models.CharField(max_length=3, validators=[MinLengthValidator(3)])
 
     def __str__(self):
-        return f'{self.number} {self.street}'
+        return f"{self.number} {self.street}"
 
 
 class Letting(models.Model):
@@ -21,11 +20,3 @@ class Letting(models.Model):
 
     def __str__(self):
         return self.title
-
-
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    favorite_city = models.CharField(max_length=64, blank=True)
-
-    def __str__(self):
-        return self.user.username
